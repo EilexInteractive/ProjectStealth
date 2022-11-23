65fcd1a3ab6937820e42fd8c5646c83a43ddc46a
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2022-11-23 14:39:56 +1100

    Create base play state

---
+++ States/PlayState.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using EilexFramework;
+using EilexFramework.GameStates;
+
+namespace EilexGame
+{
+    public class PlayState : GameState
+    {
+        public PlayState() : base("PlayState")
+        {
+        }
+
+        public override void Draw()
+        {
+            base.Draw();
+        }
+        public override void OnEnter()
+        {
+            Console.WriteLine("Hello World Please Enter");
+        }
+
+        public override void OnExit()
+        {
+            Console.WriteLine("Hello World Exit");
+        }
+
+        public override string? ToString()
+        {
+            return _StateName;
+        }
+
+        public override void Update(float dt)
+        {
+            base.Update(dt);
+        }
+    }
+}
