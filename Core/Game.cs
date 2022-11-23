384f3d2fac81262711ef3418ee400ea4dbd83b86
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2022-11-23 13:11:51 +1100

    Add properties and created the window

--- Core/Game.cs
+++ Core/Game.cs
@@ -9,9 +9,26 @@ namespace EilexFramework
         // Window Size
         public static int WindowWidth = 1200;
         public static int WindowHeight = 800;
-        public static string WindowTitle = "Eilex Game";
-        public static bool Fullscren = false;
-        public static bool IsRunning = true;
+        public static string WindowTitle = "Eilex Game";                    // Name of the window
+        public static bool FullScreen = false;                               // If full screen mode or not
+        public static bool IsRunning = true;                                // If the game is running
+
+        
+        public static void CreateWindow(int windowWidth, int windowHeight, string title, bool fullscreen = false)
+        {
+            WindowWidth = windowWidth;
+            WindowHeight = windowHeight;
+            WindowTitle = title;
+            FullScreen = fullscreen;
+
+            InitWindow(WindowWidth, WindowHeight, WindowTitle);
+
+            if(FullScreen)
+            {
+                ToggleFullscreen();
+            }
+        }
+
 
     }
 }
