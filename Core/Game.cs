0c769ef7d009393ceb56ce664cab8fb715d4fb16
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2022-11-23 13:01:59 +1100

    Created Game class with window properties

---
+++ Core/Game.cs
@@ -0,0 +1,17 @@
+using System;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+
+namespace EilexFramework
+{
+    public static class Game
+    {
+        // Window Size
+        public static int WindowWidth = 1200;
+        public static int WindowHeight = 800;
+        public static string WindowTitle = "Eilex Game";
+        public static bool Fullscren = false;
+        public static bool IsRunning = true;
+
+    }
+}
