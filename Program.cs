d6cc47b0232371f31ef73a1fba544f2aa384f1fa
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2022-11-23 12:16:54 +1100

    Initial Log

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+
+namespace ProjectStealth
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
